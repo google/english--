4a4325e6f43a4798eaf12f28152b2b570d353162
samuelgoto@gmail.com <samuelgoto@gmail.com>
2019-07-31 15:41:57 -0700

    Adds periods

--- tests/drt.js
+++ tests/drt.js
@@ -12,48 +12,74 @@ describe.only("DRT", function() {
   });
 
   let S = (np, vp) => { return {"@type": "Sentence", np: np, vp: vp} };
-  let NP = (pn) => { return {"@type": "NounPhrase", np: pn} };
+  let NP = (...args) => { return {"@type": "NounPhrase", children: args} };
   let VP = (vb, np) => { return {"@type": "VerbPhrase", verb: vb, np: np} };
   let PN = (name) => { return {"@type": "ProperName", name: name} };
   let V = (name) => { return {"@type": "Verb", name: name} };
   let PRO = (name) => { return {"@type": "Pronoun", name: name} };
+  let DET = (name) => { return {"@type": "Determiner", name: name} };
+  let N = (name) => { return {"@type": "Noun", name: name} };
 
   it.only("S = NP VP", function() {
     const parser = peg.generate(`
-      S = np:NP vp:VP {return {"@type": "Sentence", np: np, vp: vp}}
+      S = np:NP vp:VP PERIOD {return {"@type": "Sentence", np: np, vp: vp}}
       VP = v:V np:NP { return {"@type": "VerbPhrase", verb: v, np: np}; }
-      NP = pn:PN { return { "@type": "NounPhrase", np: pn}; } /
-           pro:PRO { return { "@type": "NounPhrase", np: pro}; }
+      NP = pn:PN { return { "@type": "NounPhrase", children: [pn]}; } /
+           pro:PRO { return { "@type": "NounPhrase", children: [pro]}; } /
+           det:DET n:N { return { "@type": "NounPhrase", children: [det, n]}; }
+
       PN = _ name:names _ { return {"@type": "ProperName", "name": name} }
       V = _ name:verbs _ { return {"@type": "Verb", "name": name} }
+      N = _ name:nouns _ { return {"@type": "Noun", "name": name} }
+
       PRO = _ name:pronous _ { return {"@type": "Pronoun", "name": name} }
+      DET = _ name:determiners _ { return {"@type": "Determiner", "name": name} }
 
       _ = [ ]*
 
+      PERIOD = _ "." _
+
       names = "Jones" /
               "Mary"
+
       verbs = "likes" /
               "loves"
-      pronous = "he" / "him" / "she" / "her" / "it" / "they" /
-                "He" / "Him" / "She" / "Her" / "It" / "They"
+
+      nouns = "book" /
+              "man" /
+              "woman" /
+              "donkey" /
+              "car"
+
+      determiners = "a" / "every" / "the" / "some" / "all" / "most"
+
+      pronous = "he" / "him" / "she" / "her" / "it" / "they"
 
     `);
-    assertThat(parser.parse("Jones loves Mary"))
+    assertThat(parser.parse("Jones loves Mary."))
      .equalsTo(S(NP(PN("Jones")), 
                  VP(V("loves"), 
                     NP(PN("Mary")))));
-    assertThat(parser.parse("Mary likes Jones"))
+    assertThat(parser.parse("Mary likes Jones."))
      .equalsTo(S(NP(PN("Mary")), 
                  VP(V("likes"), 
                     NP(PN("Jones")))));
-    assertThat(parser.parse("Mary likes him"))
+    assertThat(parser.parse("Mary likes him."))
      .equalsTo(S(NP(PN("Mary")), 
                  VP(V("likes"), 
                     NP(PRO("him")))));
-    assertThat(parser.parse("She likes him"))
-     .equalsTo(S(NP(PRO("She")), 
+    assertThat(parser.parse("she likes him."))
+     .equalsTo(S(NP(PRO("she")), 
                  VP(V("likes"), 
                     NP(PRO("him")))));
+    assertThat(parser.parse("she likes every car."))
+     .equalsTo(S(NP(PRO("she")), 
+                 VP(V("likes"), 
+                    NP(DET("every"), N("car")))));
+    assertThat(parser.parse("a man likes a woman."))
+     .equalsTo(S(NP(DET("a"), N("man")), 
+                 VP(V("likes"), 
+                    NP(DET("a"), N("woman")))));
    });
 
   function assertThat(x) {
