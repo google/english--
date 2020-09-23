c4c1ad5bd757dfbdbf115577859408150ba8525d
Sam Goto <samuelgoto@gmail.com>
2020-09-23 16:55:28 -0700

    Adds a test that tests the actual compiled bundle

--- tests/drt/browser.js
+++ tests/drt/browser.js
@@ -1,9 +1,24 @@
 const Assert = require("assert");
 
-const {load, parse, DRS, Rules} = require("../../src/drt/browser.js");
-
 describe("Browser", function() {
-    it("main", async function() {
+    it("source", async function() {
+        const {load, parse, DRS, Rules} = require("../../src/drt/browser.js");
+        await load("bin/", async (path) => {
+            let content = require("fs").readFileSync(path).toString();
+            return Promise.resolve({
+                text() {
+                    return Promise.resolve(content);
+                }
+            });
+        });
+        assertThat(parse("John likes Mary.").length).equalsTo(1);
+        let drs = new DRS(Rules.from());
+        assertThat(drs.feed("John likes Mary.").print())
+          .equalsTo("drs(a, b) {\nJohn(a)\nMary(b)\na likes b\n}");
+    });
+
+    it("compiled", async function() {
+        const {load, parse, DRS, Rules} = require("../../bin/bundle.js");
         await load("bin/", async (path) => {
             let content = require("fs").readFileSync(path).toString();
             return Promise.resolve({
