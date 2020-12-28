15e16f2ae733c7eea60129e8d0153c2fcf413aae
Samuel Goto <samuelgoto@gmail.com>
2020-12-27 22:37:34 -0800

    Move the autocomplete code out of the tests and into the src directory and rename it to autocomplete from errors

--- tests/drt/autocomplete.js
+++ tests/drt/autocomplete.js
@@ -5,7 +5,9 @@ const {
   FeaturedNearley,
   Parser} = require("../../src/drt/parser.js");
 
-describe("Error handling", () => {
+const {autocomplete, ancestors, valid, continuous} = require("../../src/drt/autocomplete.js");
+
+describe("Autocomplete", () => {
 
   it("Report", function() {
     let parser = Nearley.from(`
@@ -601,162 +603,6 @@ A "f" token based on:
 `.trim());
   });
 
-  function print(state) {
-    let {rule} = state;
-    let meta = {};
-    if (rule.postprocess && rule.postprocess.meta) {
-      meta = rule.postprocess.meta;
-    }
-    let features = (types) => Object
-        .entries(types)
-        .map(([key, value]) => `${key}=${value}`)
-        .join(", ");
-
-    let head = {
-      "@type": rule.name,
-      types: meta.types,
-    };
-
-    let tail = rule.symbols.map((symbol, i) => {
-      if (symbol.type) {
-        return {
-          "@type": `%${symbol.type}`
-        };
-      }
-      // console.log(meta.conditions);
-      // console.log(symbol);
-      return {
-        "@type": `${symbol}`,
-        "types": meta.conditions ? meta.conditions[i].types : undefined
-      }
-    });
-
-    let dot = state.dot;
-        
-    // console.log(meta);
-    let result = [];
-    result.push(`${head["@type"]}`);
-    if (head["types"]) {
-      result.push(`[${features(head["types"])}]`);
-    }
-    result.push(" →");
-    for (let i = 0; i < tail.length; i++) {
-      let symbol = tail[i];
-      result.push(" ");
-      if (dot == i) {
-        result.push("● ");
-      };
-      result.push(`${symbol["@type"]}`);
-      if (symbol["types"]) {
-        result.push(`[${features(symbol["types"])}]`);
-      }
-    }
-    return result.join("");
-  }
-  
-  function ancestors(state, path = []) {
-    if (state.wantedBy.length == 0) {
-      return [[state]];
-    }
-    
-    if (path.includes(state)) {
-      return [];
-    }
-    
-    let result = [];
-    path.push(state);
-    
-    if (!valid(path) || !continuous(path)) {
-      return [];
-    }
-    
-    for (let parent of state.wantedBy) {
-      for (line of ancestors(parent, path)) {
-        line.unshift(state);
-        result.push(line);
-      }
-    }
-    path.pop();
-    
-    return result;
-  }
-  
-  function walk({isComplete, data, left, right}) {
-    let result = [];
-    if (data) {
-      result.push(data);
-    }
-    if (isComplete) {
-      return result.flat();
-    }
-    if (left) {
-      result.push(...walk(left));
-    }
-    if (right) {
-      result.push(...walk(right));
-    }
-    return result.flat();
-  }
-    
-  function valid(path) {
-    for (let line of path) {
-      if (!line.rule.postprocess ||
-          !line.rule.postprocess.meta) {
-        return true;
-      }
-      const meta = line.rule.postprocess.meta; 
-      let right = walk(line);
-      let result = match(meta.type, meta.types, meta.conditions,
-                         right, undefined, false, true);
-      if (!result) {
-        return false;
-      }
-    }
-    return true;
-  }
-  
-  function continuous(path) {
-    let j = 0;
-    do  {
-      let rule = path[j].rule;
-      if (rule.postprocess && rule.postprocess.meta) {
-        break;
-      }
-      j++;
-
-      if (j >= path.length) {
-        // If we got to the end of the array
-        // with no types, this is a static path.
-        return true;
-      }
-    } while (true);
-
-    //console.log(j);
-    //console.log(path);
-    let last = {
-      "@type": path[j].rule.name,
-      "types" : path[j].rule.postprocess.meta.types,
-    };
-
-    for (let i = (j + 1); i < path.length; i++) {
-      // let current = path[i];
-      let next = path[i];
-      //console.log(j);
-      let meta = next.rule.postprocess.meta;
-      let right = walk(next);
-      right.push(last);
-      let result = match(next.rule.name, meta.types, meta.conditions,
-                   right, undefined, false, true);
-      if (!result) {
-        //console.log(meta.conditions);
-        //console.log(walk(next));
-        //console.log(`${next.rule.name} cant take ${last["@type"]}`);
-        return false;
-      }
-      last = result;
-    }
-    return true;
-  }
   
   it("Typed Tables", function() {
     let grammar = FeaturedNearley.compile(`
@@ -836,8 +682,7 @@ A "f" token based on:
     let tokens = autocomplete(parser);
 
     // There are 24 ways to start a sentence.
-    assertThat(tokens.length).equalsTo(24);
-    assertThat(tokens.map(([token]) => token)).equalsTo([
+    assertThat(Object.keys(tokens)).equalsTo([
       // conditionals
       "__if__",
       // quantifiers
@@ -870,40 +715,16 @@ A "f" token based on:
     ]);;
     
   });
-
-  function autocomplete(parser) {
-    let tokens = {};
-    for (let track of parser.parser.tracks()) {
-      for (let path of ancestors(track.stack[0])) {
-        // Saves the first valid path.
-        if (!tokens[track.symbol]) {
-          tokens[track.symbol] = path;
-        }
-      }
-    }
-
-    let completions = Object.entries(tokens);
-    
-    completions.print = () => {
-      let result = [];
-      for (let [symbol, path] of Object.entries(tokens)) {
-        result.push(`A ${symbol} token based on:`);
-        for (let line of path) {
-          result.push(`    ${print(line)}`);
-        }
-      }
-      return result.join("\n");
-    }
-    
-    return completions;
-  }
+  
+  function feed(parser, str) {
+    parser.feed(str);
+    return Object.keys(autocomplete(parser));
+  };
   
   it("Autocomplete from one token", function() {
     let parser = new Parser("Statement");
-    parser.feed("at");
-    
     // There is just a single way to follow an "at": a space.
-    assertThat(autocomplete(parser).map(([token, value]) => token)).equalsTo([
+    assertThat(feed(parser, "at")).equalsTo([
       // at has to be followed by a space!
       "WS",
     ]);
@@ -911,9 +732,8 @@ A "f" token based on:
   
   it("Autocomplete with two tokens", function() {
     let parser = new Parser("Statement");
-    parser.feed("at ");
     
-    assertThat(autocomplete(parser).map(([token, value]) => token)).equalsTo([
+    assertThat(feed(parser, "at ")).equalsTo([
       "WS",
       "least",
       "most",
@@ -922,19 +742,11 @@ A "f" token based on:
   
   it("Autocomplete with 6 tokens", function() {
     let parser = new Parser("Statement");
-    parser.feed("at least 3 ");
-    
-    assertThat(autocomplete(parser).map(([token, value]) => token)).equalsTo([
+    assertThat(feed(parser, "at least 3 ")).equalsTo([
       "WS", "word",
     ]);
   });
 
-  function feed(parser, str) {
-    parser.feed(str);
-    return autocomplete(parser).map(([token, value]) => token);
-  };
-    
-  
   it("Autocomplete streaming", function() {
     let parser = new Parser("Statement");
     assertThat(feed(parser, "at")).equalsTo(["WS"]);
