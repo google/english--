df461a30168b61547732615b31b2084d552ad8f8
Samuel Goto <samuelgoto@gmail.com>
2021-01-04 22:06:17 -0800

    Refactor verb inflection interfaces

--- tests/drt/dict.js
+++ tests/drt/dict.js
@@ -1,73 +1,78 @@
-const N = (str, types = {}) => [str, "word", [{"@type": "N", types: types}]];
+const N = (str, num, gen) => [str, "word", [{"@type": "N", "types": {"num": num, "gen": gen}}]];
 const ADJ = (str, types = {}) => [str, "word", [{"@type": "ADJ", types: types}]];
-const INF = (str, types = {}) => [str, "word", [{
+const INF = (str, trans) => [str, "word", [{
   "@type": "V",
-  "types": Object.assign({
+  "types": {
     "num": 1,
+    "trans": trans,
     "fin": "-",
     "stat": "-",
     "tense": "pres"
-  }, types)}]];
+  }}]];
 
-const PRES = (str, types = {}) => [str, "word", [{
+const PRES = (str, num, trans) => [str, "word", [{
   "@type": "V",
-  "types": Object.assign({
+  "types": {
+    "num": num,
+    "trans": trans,
     "fin": "+",
     "stat": "-",
     "tense": "pres"
-  }, types)}]];
+  }}]];
 
-const PAST = (str, types = {}) => [str, "word", [{
+const PAST = (str, trans) => [str, "word", [{
   "@type": "V",
-  "types": Object.assign({
+  "types": {
     "num": 1,
+    "trans": trans,
     "fin": "+",
     "stat": "-",
     "tense": "past",
     "tp": "-past"
-  }, types)}]];
+  }}]];
 
-const PART = (str, types = {}) => [str, "word", [{
+const PART = (str, trans) => [str, "word", [{
   "@type": "V",
-  "types": Object.assign({
+  "types": {
     "num": 1,
+    "trans": trans,
     "fin": "part",
     "stat": "-",
     "tense": ["pres", "past"],
     "tp": "-past"
-  }, types)}]];
+  }}]];
 
 const dict = [  
   // Singular Nouns
-  N("man", {"num": "sing", "gen": "male"}),
-  N("woman", {"num": "sing", "gen": "fem"}),
-  N("girl", {"num": "sing", "gen": "fem"}),
-  N("book", {"num": "sing", "gen": "-hum"}),
-  N("telescope", {"num": "sing", "gen": "-hum"}),
-  N("donkey", {"num": "sing", "gen": "-hum"}),
-  N("horse", {"num": "sing", "gen": "-hum"}),
-  N("cat", {"num": "sing", "gen": "-hum"}),
-  N("porsche", {"num": "sing", "gen": "-hum"}),
-  N("dish", {"num": "sing", "gen": "-hum"}),
-  N("witch", {"num": "sing", "gen": "-hum"}),
-  N("judge", {"num": "sing", "gen": 1}),
-  N("engineer", {"num": "sing", "gen": ["male", "fem"]}),
-  N("reservation", {"num": "sing", "gen": "-hum"}),
-  N("brazilian", {"num": "sing"}),
+  N("man", "sing", "male"),
+  N("woman", "sing", "fem"),
+  N("girl", "sing", "fem"),
+  N("book", "sing", "-hum"),
+  N("telescope", "sing", "-hum"),
+  N("donkey", "sing", "-hum"),
+  N("horse", "sing", "-hum"),
+  N("cat", "sing", "-hum"),
+  N("porsche", "sing", "-hum"),
+  N("dish", "sing", "-hum"),
+  N("witch", "sing", "-hum"),
+  N("judge", "sing", 1),
+  N("engineer", "sing", ["male", "fem"]),
+  N("reservation", "sing", "-hum"),
+  N("brazilian", "sing", 1),
 
   // Plural Nouns
-  N("brazilians", {"num": "plur"}),
-  N("men", {"num": "plur", "gen": "male"}),
-  N("women", {"num": "plur", "gen": "fem"}),
-  N("porsches", {"num": "plur", "gen": "-hum"}),
+  N("brazilians", "plur", 1),
+  N("men", "plur", "male"),
+  N("women", "plur", "fem"),
+  N("porsches", "plur", "-hum"),
   
   // RNs  
-  N("brother", {"num": "sing", "gen": "male"}),
-  N("father", {"num": "sing", "gen": "male"}),
-  N("husband", {"num": "sing", "gen": "male"}),
-  N("sister", {"num": "sing", "gen": "fem"}),
-  N("mother", {"num": "sing", "gen": "fem"}),
-  N("wife", {"num": "sing", "gen": "fem"}),
+  N("brother", "sing", "male"),
+  N("father", "sing", "male"),
+  N("husband", "sing", "male"),
+  N("sister", "sing", "fem"),
+  N("mother", "sing", "fem"),
+  N("wife", "sing", "fem"),
 
   // Adjectives
   ADJ("brazilian"),
@@ -82,49 +87,49 @@ const dict = [
   // Verb Morphology
 
   // Infinitives
-  INF("admire", {"trans": "+"}),
-  INF("fascinate", {"trans": "+"}),
-  INF("own", {"trans": "+"}),
-  INF("kiss", {"trans": "+"}),
-  INF("stink", {"trans": "-"}),
-  INF("walk", {"trans": "-"}),
-  INF("love", {"trans": ["+", "-"]}),
-  INF("like", {"trans": "+"}),
+  INF("admire", "+"),
+  INF("fascinate", "+"),
+  INF("own", "+"),
+  INF("kiss", "+"),
+  INF("stink", "-"),
+  INF("walk", "-"),
+  INF("love", ["+", "-"]),
+  INF("like", "+"),
 
   // Plural Present Tense
-  PRES("ski", {"num": "plur", "trans": "-"}),
-  PRES("like", {"num": "plur", "trans": "+"}),
-  PRES("love", {"num": "plur", "trans": ["+", "-"]}),
-  PRES("walk", {"num": "plur", "trans": "-"}),
+  PRES("ski", "plur", "-"),
+  PRES("like", "plur", "+"),
+  PRES("love", "plur", ["+", "-"]),
+  PRES("walk", "plur", "-"),
 
   // Singular Present
-  PRES("admires", {"num": "sing", "trans": "+"}),
-  PRES("likes", {"num": "sing", "trans": "+"}),
-  PRES("loves", {"num": "sing", "trans": 1}),
-  PRES("fascinates", {"num": "sing", "trans": "+"}),
-  PRES("owns", {"num": "sing", "trans": "+"}),
-  PRES("skis", {"num": "sing", "trans": "-"}),
-  PRES("walks", {"num": "sing", "trans": "-"}),
-  PRES("surprises", {"num": "sing", "trans": "+"}),
-  PRES("stinks", {"num": "sing", "trans": "-"}),
-  PRES("watches", {"num": "sing", "trans": "+"}),
+  PRES("admires", "sing", "+"),
+  PRES("likes", "sing", "+"),
+  PRES("loves", "sing", 1),
+  PRES("fascinates", "sing", "+"),
+  PRES("owns", "sing", "+"),
+  PRES("skis", "sing", "-"),
+  PRES("walks", "sing", "-"),
+  PRES("surprises", "sing", "+"),
+  PRES("stinks", "sing", "-"),
+  PRES("watches", "sing", "+"),
 
   // Past
-  PAST("made", {"trans": "+"}),
-  PAST("came", {"trans": 2}),
-  PAST("travelled", {"trans": "-"}),
-  PAST("gave", {"trans": "+"}),
-  PAST("played", {"trans": "-"}),
-  PAST("skied", {"trans": "-"}),
-  PAST("liked", {"trans": "+"}),
-  PAST("kissed", {"trans": "+"}),  
-  PAST("walked", {"trans": "-"}),
+  PAST("made", "+"),
+  PAST("came", 2),
+  PAST("travelled", "-"),
+  PAST("gave", "+"),
+  PAST("played", "-"),
+  PAST("skied", "-"),
+  PAST("liked", "+"),
+  PAST("kissed", "+"),  
+  PAST("walked", "-"),
 
   // Past Participle
-  PART("owned", {"trans": "+"}),
-  PART("loved", {"trans": "+"}),
-  PART("walked", {"trans": "-"}),
-  PART("kissed", {"trans": "+"}),
+  PART("owned", "+"),
+  PART("loved", "+"),
+  PART("walked", "-"),
+  PART("kissed", "+"),
 ];
 
 module.exports = {
