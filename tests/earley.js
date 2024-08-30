2bae784e202c71fd4766cbae10871b6597099055
Sam Goto <samuelgoto@gmail.com>
2024-08-30 13:02:55 -0700

    Almost there

--- tests/earley.js
+++ tests/earley.js
@@ -104,6 +104,17 @@ describe("Earley", function() {
     ]);
   });
 
+  function print([index, dot, state], rules) {
+    const name = rules[index][0];
+    const body = rules[index][1]
+	  .map(([type, name], i) => `${i == dot ? "• " : ""}${name}`)
+	  .join(" ");
+	
+    const completed = dot == rules[index][1].length ? " •" : "";
+    
+    return name + " -> " + body + completed + " (" + state + ")"
+  }
+  
   class Parser {
     constructor(rules = []) {
       this.rules = rules;
@@ -177,14 +188,7 @@ describe("Earley", function() {
       
       const rules = this.rules;
       return [...set].map(([index, dot, state]) => {
-	const name = rules[index][0];
-	const body = rules[index][1]
-	      .map(([type, name], i) => `${i == dot ? "• " : ""}${name}`)
-	      .join(" ");
-	
-	const completed = dot == rules[index][1].length ? " •" : "";
-      
-	return name + " -> " + body + completed + " (" + state + ")"
+	return print([index, dot, state], rules);
       });
     }
     
@@ -396,7 +400,7 @@ describe("Earley", function() {
     ]);
   });
 
-  it("Products and factors", () => {
+  it.only("Products and factors", () => {
     // Example from:
     // https://loup-vaillant.fr/tutorials/earley-parsing/recogniser
     
@@ -494,9 +498,410 @@ describe("Earley", function() {
       "Sum -> Sum • +- Product (0)",
     ]);
 
+    // console.log(parser.S);
+
+    const steps = [];
+    for (let i = 0; i < parser.S.length; i++) {
+      //console.log(`=== ${i} ===`);
+      const state = parser.S[i];
+      for (const [index, dot, step] of state) {
+	const [head, body] = parser.rules[index];
+	if (dot == body.length) {
+	  //console.log(print([index, dot, step], parser.rules));
+	  steps[step] = steps[step] || [];
+	  steps[step].unshift([index, dot, i]);
+	}
+      }
+      //console.log("");
+    }
+
+    const tree = [];
+    for (let i = 0; i < steps.length; i++) {
+      tree.push(`=== ${i} ===`);
+      for (let j = 0; j < (steps[i] || []).length; j++) {
+	tree.push(print(steps[i][j], parser.rules));
+      }
+      tree.push("");
+    }
+
+    assertThat(tree.join("\n").trim())
+      .equalsTo(`
+
+=== 0 ===
+Sum -> Sum +- Product • (3)
+Sum -> Product • (1)
+Product -> Factor • (1)
+Factor -> Number • (1)
+Number -> [0-9] • (1)
+
+=== 1 ===
+
+=== 2 ===
+Product -> Factor • (3)
+Factor -> Number • (3)
+Number -> [0-9] • (3)
+
+`.trim());
+
+    // return;
+    
+    // TODO(goto): handle multiple roots.
+    const root = steps[0].find(([index, dot, step]) => step == 3);
+
+    {
+      function leaf(node) {
+	const [step, i, dot] = node;
+	const [rule, , end] = steps[step][i];
+	const [head, body] = parser.rules[rule];
+
+	if (body.length > 1) {
+	  return false;
+	}
+	const [type] = body[0];
+	return type == "token";
+      };
+
+      function edges(node) {
+	if (leaf(node)) {
+	  return [];
+	}
+	
+	const [step, i, dot, offset] = node;
+	const [rule, , end] = steps[step][i];
+	const [head, body] = parser.rules[rule];
+
+	let name;
+	let next = dot;
+	while (next < body.length) {
+	  const [type,  head] = body[next];
+	  if (type == "term") {
+	    name = head;
+	    break;
+	  }
+	  next++;
+	};
+
+	const edges = [];
+	for (let j = 0; j < steps[next].length; j++) {
+	  const [rule, , end] = steps[next][j];
+	  const [head, body] = parser.rules[rule];
+	  if (head == name) {
+	    const edge = [next, j];
+	    edge.print = () => `${next}: ` + print(steps[next][j], parser.rules);
+	    edges.push(edge);
+	  }
+	}
+
+	return edges;
+      }
+
+      function toString(node) {
+	const [step, i, dot, offset] = node;
+	const [rule, , end] = steps[step][i];
+	return offset + ": " + print([rule, dot, end], parser.rules)
+      }
+
+      function failed(node) {
+	const [step, i, dot, offset] = node;
+	const [rule, , end] = steps[step][i];
+	const [head, body] = parser.rules[rule];
+
+	if (body.length > dot && offset >= end) {
+	  // if we aren't yet at the end of the rule
+	  // but have no characters left, this node
+	  // has failed.
+	  return true;
+	}
+
+	return false;
+      }
+
+      function move(node, edge) {
+	const [step, i, dot, begin] = node;
+	const [rule, , ] = steps[step][i];
+	const [head, body] = parser.rules[rule];
+	const [next, j] = edge;
+	const [, , end] = steps[next][j]; 
+
+	let p = dot;
+	while (p < body.length) {
+	  const [type] = body[p];
+	  if (type == "term") {
+	    break;
+	  }
+	  p++;
+	};
+
+	// console.log(p);
+	return [
+	  step, i,
+	  p + 1 /** moves forward, skipping tokens */,
+	  end /** eats the last term */
+	];
+      }
+      
+      {
+	// Each node is the step and where we are at
+	const node = [0, 4, 0, 0];
+	assertThat(toString(node))
+	  .equalsTo("0: Number -> • [0-9] (1)");
+	assertThat(leaf(node)).equalsTo(true);
+	// assertThat(parse(node))
+	//  .equalsTo([node, []]);
+	assertThat(edges(node))
+	  .equalsTo([]);
+	assertThat(failed(node)).equalsTo(false);
+      }
+
+      {
+	const node = [0, 3, 0, 0];
+	assertThat(toString(node))
+	  .equalsTo("0: Factor -> • Number (1)");
+	assertThat(leaf(node)).equalsTo(false);
+	assertThat(edges(node).map(({print}) => print()))
+	  .equalsTo(["0: Number -> [0-9] • (1)"]);
+	assertThat(failed(node)).equalsTo(false);
+      }
+
+      {
+	const node = [0, 2, 0, 0];
+	assertThat(toString(node))
+	  .equalsTo("0: Product -> • Factor (1)");
+	assertThat(leaf(node)).equalsTo(false);
+	assertThat(edges(node).map(({print}) => print()))
+	  .equalsTo(["0: Factor -> Number • (1)"]);
+	assertThat(failed(node)).equalsTo(false);
+      }
+
+      {
+	const node = [0, 1, 0, 0];
+	assertThat(toString(node))
+	  .equalsTo("0: Sum -> • Product (1)");
+	assertThat(leaf(node)).equalsTo(false);
+	assertThat(edges(node).map(({print}) => print()))
+	  .equalsTo(["0: Product -> Factor • (1)"]);
+	assertThat(failed(node)).equalsTo(false);
+      }
+
+      {
+	const node = [0, 0, 0, 0];
+	assertThat(toString(node))
+	  .equalsTo("0: Sum -> • Sum +- Product (3)");
+	assertThat(leaf(node)).equalsTo(false);
+	assertThat(edges(node).map(({print}) => print()))
+	  .equalsTo([
+	    "0: Sum -> Sum +- Product • (3)",
+	    "0: Sum -> Product • (1)",
+	  ]);
+	assertThat(failed(node)).equalsTo(false);
+
+	{
+	  const next = move(node, edges(node)[0]);
+	  assertThat(toString(next))
+	    .equalsTo("3: Sum -> Sum • +- Product (3)");
+	  assertThat(failed(next)).equalsTo(true);
+	}
+
+	{
+	  const next = move(node, edges(node)[1]);
+	  assertThat(toString(next))
+	    .equalsTo("1: Sum -> Sum • +- Product (3)");
+	  assertThat(failed(next)).equalsTo(false);
+
+	  assertThat(edges(next).map(({print}) => print()))
+	    .equalsTo(["2: Product -> Factor • (3)"]);
+
+	  assertThat(toString(move(next, edges(next)[0])))
+	    .equalsTo("3: Sum -> Sum +- Product • (3)");
+	}
+      }
+
+      {
+	const node = [0, 0, 0, 0];
+
+	function done(node) {
+	  const [step, i, dot, offset] = node;
+	  const [rule, , end] = steps[step][i];
+	  const [head, body] = parser.rules[rule];
+
+	  // console.log(toString(node));
+	  //console.log(`body=${body.length} dot=${dot}: ${toString(node)}`);
+	  if (body.length == dot) {
+	    return true;
+	  }
+
+	  return false;
+	}
+	
+	function parse(node) {
+	  if (leaf(node)) {
+	    return [node];
+	  }
+
+	  if (done(node)) {
+	    return [];
+	  }
+
+	  if (failed(node)) {
+	    return false;
+	  }
+
+	  for (const edge of edges(node)) {
+	    const next = move(node, edge);
+	    const tail = parse(next);
+	    if (!tail) {
+	      continue;
+	    }
+	    const [step, i] = edge;
+	    const child = [step, i, 0, step];
+	    return [[edge, parse(child)], tail];
+	  }
+	}
+
+	assertThat(parse(node)).equalsTo();
+	
+	assertThat(parse(node).map(([edge, tree]) => edge.print()))
+	  .equalsTo([
+	    "0: Sum -> Product • (1)",
+	    "2: Product -> Factor • (3)"
+	  ]);
+	//assertThat(parse(node).map(([edge, tree]) => tree.map(([edge, tree]) => tree)))
+	//  .equalsTo([
+	//    //"0: Sum -> Product • (1)",
+	//    //"2: Product -> Factor • (3)"
+	//  ]);
+      }
+    }
+
+    return;
+    
+    assertThat(print(root, parser.rules))
+      .equalsTo("Sum -> Sum +- Product • (3)");
+
+    function parse(rule, dot) {
+      const [index, , step] = rule;
+      const [head, body] = parser.rules[index];
+      // console.log(body);
+      console.log(body[0]);
+
+      
+      
+      console.log(print([index, dot, step], parser.rules));
+    }
+    
+    assertThat(parse(steps[0][4], 0)).equalsTo();
+    
+    return;
+    
+    function search(parent, offset, begin) {
+      const [index, dot, end] = parent;
+      console.log(`Searching for the ${offset}th child of step #${begin} ${print(parent, parser.rules)} starting from ${begin}`);
+      const [head, body] = parser.rules[index];
+
+      if (begin < end && offset < body.length) {
+	const [type, name] = body[offset];
+	if (type == "token") {
+	  if ((begin + 1) >= end) {
+	    return [["token", name]];
+	  }
+	  const next = search(parent, offset + 1, begin + 1);
+	  //  if (next) {
+	  return [["token", name], next];
+	  //}
+	  //return ["token", name];
+	  // return name;
+	}
+
+	// console.log("hi");
+      }
+      
+      // for (const child of body) {
+      // console.log(body[offset]);
+      if (begin >= end) {
+	// throw new Error(`Can't find a child: reached the end of the symbols: ${begin} >= ${end}`);
+	// throw new Error("hi");
+	// console.log(`${begin} ${end}`);
+	return false;
+      }
+            
+      const [type, name] = body[offset];
+      // Completions starting at this index:
+      const completions = steps[begin]
+	    .filter(([index]) => parser.rules[index][0] == name);
+      for (const completion of completions) {
+	// console.log(print(completion, parser.rules));
+	const [, , e] = completion;
+	// console.log(e);
+	console.log(`Step #${begin}: ` + print(completion, parser.rules));
+	// This is the last symbol to search
+	//if ((offset + 1) == body.length &&
+	//   ) {
+	//}
+	// console.log(`${(offset + 1) == body.length} `);
+	// console.log(`${e} ${end} `);
+	// We are at the end, and the endings match
+	// console.log(`${offset} ${body.length} ${e} ${end}`);
+	// console.log(head);
+	// console.log();
+	// console.log("hi");
+	if ((offset + 1) == body.length && end == e) {
+	  console.log("hello: " + head);
+	  // console.log(completion);
+	  // console.log(begin);
+	  // throw new Error("hi");
+	  // const children = ;
+	  // console.log(children);
+	  //
+	  return ["term", head, search(completion, 0, begin)];
+	}
+
+	console.log("hello");
+	const next = search(parent, offset + 1, e);
+	if (next) {
+	  return [["term", head, search(completion, 0, begin)], ...next];
+	}
+      }
+    }
+
+    console.log(search([4, 3, 1], 0, 2));
+
+    return;
+    
+    const result = search(root, 0, 0);
+    // console.log(JSON.stringify(result, undefined, 2));
+    return;
+    assertThat(result).equalsTo([
+      ["term", "Sum", [
+	"term", "Sum", [
+	  "term", "Product", [
+	    "term", "Factor", [
+	      "token", "[0-9]"
+	    ]
+	  ]
+	]
+      ]],
+      ["token", "+-"],
+      ["term", "Sum", [
+	"term", "Product", [
+	  "term", "Factor", [
+	    "token", "[0-9]"
+	  ]
+	]
+      ]]
+    ]);
+    //assertThat(print(result[0][1], parser.rules))
+    // .equalsTo("Sum -> Product • (1)");
+    // return;
+    // assertThat(print(result[2], parser.rules))
+    //  .equalsTo("Product -> Factor • (3)");
+
+    // const level2 = search(result[0], 0, 0);
   });
 
   it("Null", () => {
+    // Example from:
+    // https://loup-vaillant.fr/tutorials/earley-parsing/recogniser
+
     const parser = new Parser([
       ["$", [term("A")]],
       ["A", []],
